c8bfcc190a9a148d5d4b834b082394b1742dce36
LACERBA <hzher@qq.com>
2014-03-30 22:20:50 +0800

    测试修改一

--- StringBuilder.js
+++ StringBuilder.js
@@ -2,6 +2,10 @@
 模拟 C#中的StringBuilder,提升JS中字符串拼接的效率及性能。
 */
 
+function Test(){
+    return 'hello';
+}
+
 //对象定义（构造函数)
 //参数：所有参数须为字符串形式.
 //当参数长度为1时，参数值将是字符串之间连接的分隔符
@@ -24,7 +28,7 @@ function StringBuilder() {
 
 //向对象中添加字符串
 //参数：一个字符串值
-StringBuilder.prototype.Append = function (str) {
+StringBuilder.prototype.Append = function(str) {
     this._length += str.length;
     this._buffers[this._buffers.length] = str;
 }
@@ -34,7 +38,7 @@ StringBuilder.prototype.Add = StringBuilder.prototype.append;
 //参数：参数一是预格式化的字符串，如：'{0} {1} {2}'
 //格式参数可以是数组，或对应长度的arguments,
 //参见示例
-StringBuilder.prototype.AppendFormat = function () {
+StringBuilder.prototype.AppendFormat = function() {
     if (arguments.length > 1) {
         var TString = arguments[0];
         if (arguments[1] instanceof Array) {
@@ -43,8 +47,7 @@ StringBuilder.prototype.AppendFormat = function () {
                 var re = eval("/\\{" + jIndex + "\\}/g;");
                 TString = TString.replace(re, arguments[1][i]);
             }
-        }
-        else {
+        } else {
             for (var i = 1, iLen = arguments.length; i < iLen; i++) {
                 var jIndex = i - 1;
                 var re = eval("/\\{" + jIndex + "\\}/g;");
@@ -52,37 +55,34 @@ StringBuilder.prototype.AppendFormat = function () {
             }
         }
         this.Append(TString);
-    }
-    else if (arguments.length == 1) {
+    } else if (arguments.length == 1) {
         this.Append(arguments[0]);
     }
 }
 
 //字符串长度（相当于ToString()后输出的字符串长度
-StringBuilder.prototype.Length = function () {
+StringBuilder.prototype.Length = function() {
     if (this._splitChar.length > 0 && (!this.IsEmpty())) {
         return this._length + (this._splitChar.length * (this._buffers.length - 1));
-    }
-    else {
+    } else {
         return this._length;
     }
 }
 //字符串是否为空
-StringBuilder.prototype.IsEmpty = function () {
+StringBuilder.prototype.IsEmpty = function() {
     return this._buffers.length <= 0;
 }
 //清空
-StringBuilder.prototype.Clear = function () {
+StringBuilder.prototype.Clear = function() {
     this._buffers = [];
     this._length = 0;
 }
 //输出
 //参数：可以指定一个字符串（或单个字符），作为字符串拼接的分隔符
-StringBuilder.prototype.ToString = function () {
+StringBuilder.prototype.ToString = function() {
     if (arguments.length == 1) {
         return this._buffers.join(arguments[1]);
-    }
-    else {
+    } else {
         return this._buffers.join(this._splitChar);
     }
 }
